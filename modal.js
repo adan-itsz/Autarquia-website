273f56252be045b10ae3b271c1b5c36dc182e6bb
Adan De Santiago Avalos <adan1995a@gmail.com>
2017-04-17 18:10:42 -0500

    flayer convocatoria

---
+++ modal.js
@@ -0,0 +1,15 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
